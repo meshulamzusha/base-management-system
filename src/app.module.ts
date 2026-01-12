094fc3dd6a21c76bf6d2569726427d2f5f2b5d48
Your Name <you@example.com>
2026-01-12 13:43:01 +0200

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ShiftsModule } from './shifts/shifts.module';
+import { AssignmentsModule } from './assignments/assignments.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, ShiftsModule, AssignmentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
